e92994f99574cf9b264d855869b5e1a2878a1a4f
Loring Dodge <loringdodge@yahoo.com>
2015-11-27 14:55:42 -0600

    Add angular example

---
+++ angular/js/app.js
@@ -0,0 +1,59 @@
+angular.module('starter', ['promise-queue'])
+
+  .directive('wordSlide', function(PromiseQueue) {
+
+    return {
+      restrict: 'E',
+      link: function($scope, $element, $attrs) {
+
+        /* Listen for a transition! */
+        var transitionEvent = whichTransitionEvent();
+
+        var elements = $element.children();
+
+        var promiseQueue = new PromiseQueue();
+
+        var slideIn = function(el){
+          return function(done) {
+            angular.element(el).addClass('enter').one(transitionEvent, function(e) {
+              done();
+            });
+          }
+        }
+
+        promiseQueue
+          .add(slideIn(elements[0]))
+          .add(slideIn(elements[1]))
+          .add(slideIn(elements[2]))
+          .add(slideIn(elements[3]))
+          .add(slideIn(elements[4]))
+          .add(slideIn(elements[5]))
+          .add(slideIn(elements[6]))
+          .add(slideIn(elements[7]));
+
+          var startButton = document.getElementById('start');
+          angular.element(startButton).on('click', function() {
+            promiseQueue.start();
+          });
+
+          function whichTransitionEvent(){
+            var t;
+            var el = document.createElement('fakeelement');
+            var transitions = {
+              'transition':'transitionend',
+              'OTransition':'oTransitionEnd',
+              'MozTransition':'transitionend',
+              'WebkitTransition':'webkitTransitionEnd'
+            }
+
+            for(t in transitions){
+              if( el.style[t] !== undefined ){
+                return transitions[t];
+              }
+            }
+          }
+
+      }
+    }
+
+  });
